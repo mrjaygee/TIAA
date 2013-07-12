1b6165835ee372c589ba2f2ce16dcf633330bf42
Jason Grant <mrjaygee@gmail.com>
2013-07-12 17:06:54 -0600

    updates 07.12.2013

---
+++ includes/header.php
@@ -0,0 +1,43 @@
+<header>
+	<div id="top-elements" class="row">
+		<div class="large-7 columns">
+			<img src="img/ABC_logo.png" id="logo">
+		</div>
+		<div id="location-profile" class="large-5 columns">
+			<div class="large-6 columns border-r">
+				<h6>ABC Organization Location 1</h6>
+				<p><a href="#" class="arrow-r">change location</a></p>
+			</div>
+			<div class="large-6 columns">
+				<h6>Anne Smith</h6>
+				<p><a href="#" class="arrow-r">Your Profile</a> <a href="#" class="arrow-r">Log Out</a></p>
+			</div>
+		</div>
+	</div>
+	<div id="main-nav">
+		<div class="row">
+			<div class="large-12 columns">		
+				<nav class="top-bar">
+					<section class="top-bar-section">
+						<ul class="left">
+							<li><a href="#">Plan Summary</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Manage Contributions</a>
+							<li class="divider"></li>
+							<li class="has-dropdown"><a href="#">Plan History</a>
+								<ul class="dropdown">
+									<li class=""><a href="#">Contribution History</a></li>
+									<li class=""><a href="#">Withdrawal History</a></li>
+									<li class=""><a href="#">Allocation History</a></li>
+									<li class=""><a href="#">Communications</a></li>
+								</ul>
+							</li>
+							<li class="divider"></li>
+							<li><a href="#">Planning Tools &amp; Resources</a></li>
+						</ul>
+					</section>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
